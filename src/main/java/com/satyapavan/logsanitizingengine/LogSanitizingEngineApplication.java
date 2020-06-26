61f8c57c793b34b4ef5efa6049b968ea8ebd545b
Satya Pavan <pavan.simham@gmail.com>
2020-06-27 02:21:26 +0530

    spring starter kit version

---
+++ src/main/java/com/satyapavan/logsanitizingengine/LogSanitizingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.satyapavan.logsanitizingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSanitizingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogSanitizingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satyapavan/logsanitizingengine/LogSanitizingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyapavan.logsanitizingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogSanitizingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
