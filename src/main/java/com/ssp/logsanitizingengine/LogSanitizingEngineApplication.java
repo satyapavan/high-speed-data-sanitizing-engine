50070188050af12a4ed46ea83a8f5837aea662f6
Satya Pavan <pavan.simham@gmail.com>
2020-06-28 01:19:59 +0530

    renamed the package

--- src/main/java/com/ssp/logsanitizingengine/LogSanitizingEngineApplication.java
+++ src/main/java/com/ssp/logsanitizingengine/LogSanitizingEngineApplication.java
@@ -1,4 +1,4 @@
-package com.satyapavan.logsanitizingengine;
+package com.ssp.logsanitizingengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ssp/logsanitizingengine/LogSanitizingEngineApplicationTests.java
+++ src/test/java/com/ssp/logsanitizingengine/LogSanitizingEngineApplicationTests.java
@@ -1,4 +1,4 @@
-package com.satyapavan.logsanitizingengine;
+package com.ssp.logsanitizingengine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
