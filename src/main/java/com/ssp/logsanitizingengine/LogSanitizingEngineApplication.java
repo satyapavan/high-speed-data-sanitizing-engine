c928e9c5afefbe0486c96dee3547750a7d5e6dc4
Satya Pavan <pavan.simham@gmail.com>
2020-07-03 14:40:26 +0530

    a nice working model

--- src/main/java/com/ssp/logsanitizingengine/LogSanitizingEngineApplication.java
+++ src/main/java/com/ssp/logsanitizingengine/LogSanitizingEngineApplication.java
@@ -10,9 +10,14 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class LogSanitizingEngineApplication implements CommandLineRunner {
+
+	final String DATA_FILE = "src/resources/generated.logx";
+	final int NO_OF_ITERATIONS = 3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogSanitizingEngineApplication.class, args).close();
@@ -21,30 +26,67 @@ public class LogSanitizingEngineApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
-		System.out.println(args);
 		LogSanitizingEngineService objService = new LogSanitizingEngineService();
-		
+		objService.loadMaskedData();
+
 		/*
 		 *  http://zetcode.com/java/inputstream/
 		 */
-			
-		String fileName = "src/resources/generated.logx";
 
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		nf.setMinimumIntegerDigits(6);
+		nf.setGroupingUsed(false);
+		
 		StopWatch sw = new org.springframework.util.StopWatch();
-		sw.start("Method-1"); // Start a named task
-		
-        try (InputStream fis = new FileInputStream(fileName);
-                InputStreamReader isr = new InputStreamReader(fis,
-                        StandardCharsets.UTF_8);
-                BufferedReader br = new BufferedReader(isr)) {
+		StringBuilder sbReport = new StringBuilder();
+		AtomicInteger lineCounter = new AtomicInteger(1); 
 
-            br.lines().forEach(line -> objService.doSanitize(line));
-        }
-        
-        sw.stop();
-        
-        System.out.println("Total time in milliseconds for all tasks :"+sw.getTotalTimeMillis());
-        System.out.println("Table describing all tasks performed :\n"+sw.prettyPrint());
-		
+		sbReport.append("Record No,Data Length,");
+		for(int itr=1; itr <= NO_OF_ITERATIONS; itr++) {
+			sbReport.append("BuiltIn Pass-" + itr + ",");
+		}
+
+		for(int itr=1; itr <= NO_OF_ITERATIONS; itr++) {
+			sbReport.append("StringUtils Pass-" + itr + ",");
+		}
+
+		try (InputStream fis = new FileInputStream(DATA_FILE);
+				InputStreamReader isr = new InputStreamReader(fis,
+						StandardCharsets.UTF_8);
+				BufferedReader br = new BufferedReader(isr)) {
+
+			br.lines().forEach(line -> {
+				int passCounter = 1;
+				String tempLineNumber = "Line-" + lineCounter.getAndIncrement();
+				sbReport.append(tempLineNumber).append(",").append(line.length()).append(",");
+
+				do {
+					sw.start(tempLineNumber + "-Pass-" + Integer.toString(passCounter) + "-BuiltIn");
+					objService.doSanitizeBuiltIn(line);
+					sw.stop();
+
+					sbReport.append(nf.format(sw.getLastTaskTimeMillis())).append(",");
+
+					passCounter++;
+				} while(passCounter <= NO_OF_ITERATIONS);
+
+				passCounter = 1;
+				do {
+					sw.start(tempLineNumber + "-Pass-" + Integer.toString(passCounter)  + "-StringUtils");
+					objService.doSanitizeStringUtils(line);
+					sw.stop();
+
+					sbReport.append(nf.format(sw.getLastTaskTimeMillis())).append(",");
+
+					passCounter++;
+				} while(passCounter <= NO_OF_ITERATIONS);
+
+				sbReport.append("\n");
+			});
+		}
+
+		System.out.println("Table describing all tasks performed :\n"+sw.prettyPrint());
+		System.out.println("Total time in milliseconds for all tasks :"+sw.getTotalTimeMillis());
+		System.out.println(sbReport);
 	}
 }
--- src/main/java/com/ssp/logsanitizingengine/LogSanitizingEngineService.java
+++ src/main/java/com/ssp/logsanitizingengine/LogSanitizingEngineService.java
@@ -1,9 +1,47 @@
 package com.ssp.logsanitizingengine;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import org.springframework.util.StringUtils;
+
 public class LogSanitizingEngineService {
 
-	boolean doSanitize(String data) {
-		System.out.println(data);
+	final String MASK_DATA_FILE="src/resources/mask.data";
+	
+	HashMap<String, String> objMaskedData = new HashMap<String, String>();
+
+	boolean doSanitizeBuiltIn(String data) {
+		objMaskedData.forEach((k_mask, v_unmask) -> {
+			data.replace(k_mask, v_unmask);
+		});
+		return true;
+	}
+
+	boolean doSanitizeStringUtils(String data) {
+		objMaskedData.forEach((k_mask, v_unmask) -> {
+			StringUtils.replace(data, k_mask, v_unmask);
+		});
+		return true;
+	}
+
+	boolean loadMaskedData() throws FileNotFoundException, IOException, Exception {
+		try (InputStream fis = new FileInputStream(MASK_DATA_FILE);
+				InputStreamReader isr = new InputStreamReader(fis,
+						StandardCharsets.UTF_8);
+				BufferedReader br = new BufferedReader(isr)) {
+
+			br.lines().forEach(line -> {
+				objMaskedData.put(line, line.replaceAll("([a-z]|[A-Z]|[0-9])", "X"));
+			});
+		}
+		
+		System.out.println(objMaskedData.toString());
 		return true;
 	}
 }
