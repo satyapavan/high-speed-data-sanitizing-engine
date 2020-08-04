a54d53f72846c8551ade1b68c2fd05f499e3888a
Satya Pavan <pavan.simham@gmail.com>
2020-08-04 16:32:49 +0530

    trying d3 implementation

---
+++ web/6b2dbe02ff43d031@198.js
@@ -0,0 +1,90 @@
+// https://observablehq.com/@d3/radial-tidy-tree@198
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  const fileAttachments = new Map([["flare-2.json",new URL("./files/e65374209781891f37dea1e7a6e1c5e020a3009b8aedf113b4c80942018887a1176ad4945cf14444603ff91d3da371b3b0d72419fa8d2ee0f6e815732475d5de",import.meta.url)]]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], function(md){return(
+md`# Radial Tidy Tree
+
+D3’s [tree layout](https://github.com/d3/d3-hierarchy/blob/master/README.md#tree) implements the [Reingold–Tilford “tidy” algorithm](http://reingold.co/tidier-drawings.pdf) for constructing hierarchical node-link diagrams, improved to run in linear time by [Buchheim *et al.*](http://dirk.jivas.de/papers/buchheim02improving.pdf) Tidy trees are typically more compact than [cluster dendrograms](/@d3/radial-dendrogram), which place all leaves at the same level. See also the [Cartesian variant](/@d3/tidy-tree).`
+)});
+  main.variable(observer("chart")).define("chart", ["tree","data","d3","autoBox"], function(tree,data,d3,autoBox)
+{
+  const root = tree(data);
+  
+  const svg = d3.create("svg");
+
+  svg.append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#555")
+      .attr("stroke-opacity", 0.4)
+      .attr("stroke-width", 1.5)
+    .selectAll("path")
+    .data(root.links())
+    .join("path")
+      .attr("d", d3.linkRadial()
+          .angle(d => d.x)
+          .radius(d => d.y));
+  
+  svg.append("g")
+    .selectAll("circle")
+    .data(root.descendants())
+    .join("circle")
+      .attr("transform", d => `
+        rotate(${d.x * 180 / Math.PI - 90})
+        translate(${d.y},0)
+      `)
+      .attr("fill", d => d.children ? "#555" : "#999")
+      .attr("r", 2.5);
+
+  svg.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-width", 3)
+    .selectAll("text")
+    .data(root.descendants())
+    .join("text")
+      .attr("transform", d => `
+        rotate(${d.x * 180 / Math.PI - 90}) 
+        translate(${d.y},0) 
+        rotate(${d.x >= Math.PI ? 180 : 0})
+      `)
+      .attr("dy", "0.31em")
+      .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+      .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+      .text(d => d.data.name)
+    .clone(true).lower()
+      .attr("stroke", "white");
+
+  return svg.attr("viewBox", autoBox).node();
+}
+);
+  main.variable(observer("autoBox")).define("autoBox", function(){return(
+function autoBox() {
+  document.body.appendChild(this);
+  const {x, y, width, height} = this.getBBox();
+  document.body.removeChild(this);
+  return [x, y, width, height];
+}
+)});
+  main.variable(observer("data")).define("data", ["d3","FileAttachment"], async function(d3,FileAttachment){return(
+d3.hierarchy(await FileAttachment("flare-2.json").json())
+    .sort((a, b) => d3.ascending(a.data.name, b.data.name))
+)});
+  main.variable(observer("tree")).define("tree", ["d3","radius"], function(d3,radius){return(
+d3.tree()
+    .size([2 * Math.PI, radius])
+    .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth)
+)});
+  main.variable(observer("width")).define("width", function(){return(
+954
+)});
+  main.variable(observer("radius")).define("radius", ["width"], function(width){return(
+width / 2
+)});
+  main.variable(observer("d3")).define("d3", ["require"], function(require){return(
+require("d3@5")
+)});
+  return main;
+}
